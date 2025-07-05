75a3891b05ef6315af0a0ab0ce373f999c810c6e
Stefanus Michael S <michaelsondjaja@gmail.com>
2025-07-05 23:47:29 +0200

    update pjit and gpu,job

--- JaxJitSingleProcessOnly.py
+++ JaxJitSingleProcessOnly.py
@@ -39,8 +39,8 @@ def Collide(g):
     return g
 
 # dimensions of the 2D lattice and the Lattice parameters
-NX=1024 #int(input("nx = "))
-NY=1024 #int(input("ny = "))
+NX=40000 #int(input("nx = "))
+NY=40000 #int(input("ny = "))
 # simulation parameters
 scale  = 1               # set simulation size
 #NX     = 32*scale        # domain size
