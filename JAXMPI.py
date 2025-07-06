d6c52ee9ce99776132235ddaf880d41b5c9cff22
Stefanus Michael S <michaelsondjaja@gmail.com>
2025-07-06 22:39:27 +0200

    resize jaxmpi

--- JAXMPI.py
+++ JAXMPI.py
@@ -40,8 +40,8 @@ print(f"Process {jax.process_index()} on {socket.gethostname()} using {jax.local
 print(f"JAX backend: {jax.default_backend()}")
 
 # ✅ Simulation parameters
-NX, NY = 40, 40
-NSTEPS = 200
+NX, NY = 4000, 4000
+NSTEPS = 10000
 omega = 1.7
 u_max = 0.1
 nu = (1 / omega - 0.5) / 3
