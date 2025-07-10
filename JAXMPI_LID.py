b36eb2ca7a2655a94e7c02350ba5eb08ffd130f6
Stefanus Michael S <michaelsondjaja@gmail.com>
2025-07-10 09:03:31 +0200

    reduce domain size

--- JAXMPI_LID.py
+++ JAXMPI_LID.py
@@ -39,7 +39,7 @@ print(f"Process {jax.process_index()} on {socket.gethostname()} using {jax.local
 print(f"JAX backend: {jax.default_backend()}")
 
 # ✅ Simulation parameters
-NX, NY = 30000, 30000
+NX, NY = 10000, 10000
 NSTEPS = 5000 
 omega = 1.3
 u_max = 0.1
