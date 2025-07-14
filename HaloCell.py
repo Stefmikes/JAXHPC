cfceba2dcfa3f5de626360488ddc66a3764c61f8
Stefanus Michael S <michaelsondjaja@gmail.com>
2025-07-15 00:21:25 +0200

    increase domain size

--- HaloCell.py
+++ HaloCell.py
@@ -41,7 +41,7 @@ print(f"Process {jax.process_index()} on {socket.gethostname()} using {jax.local
 print(f"JAX backend: {jax.default_backend()}")
 
 # ✅ Simulation parameters
-NX, NY = 1000, 1000
+NX, NY = 15000, 10000
 NSTEPS = 10000
 omega = 1.3
 u_max = 0.1
