7a5b545c82e2147bae26568f70bbf74ba1de8ad6
Stefanus Michael S <michaelsondjaja@gmail.com>
2025-07-15 22:34:26 +0200

    test jax only

--- JAXMPI_LID.py
+++ JAXMPI_LID.py
@@ -39,7 +39,7 @@ print(f"Process {jax.process_index()} on {socket.gethostname()} using {jax.local
 print(f"JAX backend: {jax.default_backend()}")
 
 # ✅ Simulation parameters
-NX, NY = 300, 300
+NX, NY = 3000, 3000
 NSTEPS = 10000
 omega = 1.6
 u_max = 0.1
