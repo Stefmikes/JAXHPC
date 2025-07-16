15033032ffe3df8cb601539b22617c92294f4390
Stefanus Michael S <michaelsondjaja@gmail.com>
2025-07-16 08:23:17 +0200

    resize halo jax

--- HaloCell.py
+++ HaloCell.py
@@ -41,7 +41,7 @@ print(f"Process {jax.process_index()} on {socket.gethostname()} using {jax.local
 print(f"JAX backend: {jax.default_backend()}")
 
 # ✅ Simulation parameters
-NX, NY = 3000, 3000
+NX, NY = 30000, 30000
 NSTEPS = 10000
 omega = 1.6
 u_max = 0.1
