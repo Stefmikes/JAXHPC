04df99bd52eff16b34c250cae5d74399bc663412
Stefanus Michael S <michaelsondjaja@gmail.com>
2025-07-17 15:14:51 +0200

    resize domain to 10k 10k

--- HaloCell.py
+++ HaloCell.py
@@ -40,7 +40,7 @@ print(f"Process {jax.process_index()} on {socket.gethostname()} using {jax.local
 print(f"JAX backend: {jax.default_backend()}")
 
 # ✅ Simulation parameters
-NX, NY = 300, 300
+NX, NY = 10000, 10000
 NSTEPS = 10000
 omega = 1.7
 u_max = 0.1
