bef800082731362fcbf7addf8381a19de22daa2a
Stefanus Michael S <michaelsondjaja@gmail.com>
2025-07-17 22:00:43 +0200

    disable communicate

--- jaxwmpi.py
+++ jaxwmpi.py
@@ -282,20 +282,20 @@ with mesh:
 
         f = lbm_stream(f)
 
-        comm_cart.barrier()
-        if size > 1 and (not is_left_edge or not is_right_edge):
-            f = communicate(
-                f, comm_cart, 
-                left_src, left_dst, 
-                right_src, right_dst)
-        comm_cart.barrier() 
-        if not is_left_edge:
-            diff_left = jnp.abs(f[:,1,:] - f[:,0,:])
-            print(f"[DEBUG STEP:{step}] [Rank {rank}] Max left halo mismatch: {diff_left.max()}")
+        # comm_cart.barrier()
+        # if size > 1 and (not is_left_edge or not is_right_edge):
+        #     f = communicate(
+        #         f, comm_cart, 
+        #         left_src, left_dst, 
+        #         right_src, right_dst)
+        # comm_cart.barrier() 
+        # if not is_left_edge:
+        #     diff_left = jnp.abs(f[:,1,:] - f[:,0,:])
+        #     print(f"[DEBUG STEP:{step}] [Rank {rank}] Max left halo mismatch: {diff_left.max()}")
         
-        if not is_right_edge:
-            diff_right = jnp.abs(f[:,-2,:] - f[:,-1,:])
-            print(f"[DEBUG STEP:{step}] [Rank {rank}] Max right halo mismatch: {diff_right.max()}")
+        # if not is_right_edge:
+        #     diff_right = jnp.abs(f[:,-2,:] - f[:,-1,:])
+        #     print(f"[DEBUG STEP:{step}] [Rank {rank}] Max right halo mismatch: {diff_right.max()}")
         
 
         if step % 100 == 0:
