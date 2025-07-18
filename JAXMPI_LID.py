03e6591baef7588d54ed3c4ad71448700bb36fe2
Stefanus Michael S <michaelsondjaja@gmail.com>
2025-07-18 23:21:09 +0200

    resize domain to 6k 6k

--- JAXMPI_LID.py
+++ JAXMPI_LID.py
@@ -39,7 +39,7 @@ print(f"Process {jax.process_index()} on {socket.gethostname()} using {jax.local
 print(f"JAX backend: {jax.default_backend()}")
 
 # ✅ Simulation parameters
-NX, NY = 11000, 11000
+NX, NY = 6000, 6000
 NSTEPS = 10000
 omega = 1.6
 u_max = 0.1
